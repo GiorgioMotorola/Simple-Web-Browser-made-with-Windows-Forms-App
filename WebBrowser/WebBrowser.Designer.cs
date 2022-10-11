33d9d257ca84e2e4baf52a1f475906d1195474c3
Mike Weatherford <mikesweatherford@gmail.com>
2022-10-11 10:53:22 -0400

    first commit

---
+++ WebBrowser/WebBrowser.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WebBrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchField = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.PrevButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // SearchField
+            // 
+            this.SearchField.Location = new System.Drawing.Point(140, 28);
+            this.SearchField.Multiline = true;
+            this.SearchField.Name = "SearchField";
+            this.SearchField.Size = new System.Drawing.Size(711, 20);
+            this.SearchField.TabIndex = 0;
+            this.SearchField.Text = "https://codekentucky.org/";
+            this.SearchField.TextChanged += new System.EventHandler(this.SearchField_TextChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(216, 28);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(8, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // PrevButton
+            // 
+            this.PrevButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrevButton.Location = new System.Drawing.Point(24, 26);
+            this.PrevButton.Name = "PrevButton";
+            this.PrevButton.Size = new System.Drawing.Size(36, 23);
+            this.PrevButton.TabIndex = 2;
+            this.PrevButton.Text = "<--";
+            this.PrevButton.UseVisualStyleBackColor = true;
+            this.PrevButton.Click += new System.EventHandler(this.PrevButton_Click);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextButton.Location = new System.Drawing.Point(80, 26);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(36, 23);
+            this.NextButton.TabIndex = 3;
+            this.NextButton.Text = "-->";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.SearchButton.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.SearchButton.Location = new System.Drawing.Point(857, 29);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(67, 20);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // browser
+            // 
+            this.browser.Location = new System.Drawing.Point(12, 71);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(912, 379);
+            this.browser.TabIndex = 5;
+            this.browser.Url = new System.Uri("https://codekentucky.org/", System.UriKind.Absolute);
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            // 
+            // WebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(936, 462);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.PrevButton);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.SearchField);
+            this.Name = "WebBrowser";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SearchButton_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchField;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button PrevButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
---
+++ WebBrowser/WebBrowser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class WebBrowser : Form
+    {
+        public WebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void WebBrowser_Load(object sender, EventArgs e)
+        {
+            string url = "http://www.google.com/";
+            browser.Navigate(url);
+        }
+
+        private void PrevButton_Click(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void SearchField_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            browser.Navigate(SearchField.Text);
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            SearchField.Text = browser.Document.Url.ToString();
+            this.Text = browser.Document.Title;
+        }
+    }
+}
